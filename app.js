ed0b6ab06544e5de51e2a22536611fda0ff2b757
TheHumanMachine <Jungleman22@Comcast.net>
2020-12-20 22:15:29 -0800

    Changed port

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.get('/', (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-    console.log("listening on port: " + 3000);
+app.listen(port, () => {
+    console.log("listening on port: " + port);
 })
 
