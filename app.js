e0d40e804e7eb7fe68ed1206a566a28fb71de765
TheHumanMachine <Jungleman22@Comcast.net>
2020-12-22 18:34:05 -0800

    Adding cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
-
 const port = process.env.port || 3000;
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.send("Hello, World!");
+    res.send("Hello, World! Now with CORS!");
 });
 
 app.listen(port, () => {
